b9a0df57ebf0b388da4883aca16067e5c9d7115a
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-10-05 09:59:13 +0900

    update

--- download.sh
+++ download.sh
@@ -3,8 +3,11 @@
 set -euxo pipefail
 
 filename=README.md
+workflow=test
+branch=artifact
+artifact_name=artifact
 
-body=$(gh run list -b artifact -L 1 --json headSha,databaseId --jq '.[0]')
+body=$(gh run list -w "$workflow" -b "$branch" -L 1 --json headSha,databaseId --jq '.[0]')
 run_id=$(echo "$body" | jq -r ".databaseId")
 head_sha=$(echo "$body" | jq -r ".headSha")
 
@@ -15,7 +18,7 @@ echo "head_sha: $head_sha" >&2
 tempdir=$(mktemp -d)
 echo "tempdir: $tempdir" >&2
 
-gh run download -D "$tempdir" -n "artifact" "$run_id"
+gh run download -D "$tempdir" -n "$artifact_name" "$run_id"
 ls "$tempdir"
 tree "$tempdir"
 cat "$tempdir/$filename"
