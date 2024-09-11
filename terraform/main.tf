fea9014b808add79cf72ad449113e56cf06cd5dc
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-09-11 22:37:56 +0900

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,15 @@
+// hello
+
+
+locals {
+  unused = "test"
+}
+
 resource "null_resource" "foo-bar" {}
+
+output "no_description" {
+  value = "value"
+}
 
 terraform {
   required_version = ">= 1.0"
